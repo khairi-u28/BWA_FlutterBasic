c6ec5e0af53a164dd350478523c76bf2ef1eaae9
khairi-u28 <khairi.u28@gmail.com>
2021-02-14 13:47:11 +0700

    first commit

---
+++ lib/custom_colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color darkGreen = Color(0XFF218c74);
+Color darkGrey = Color(0XFF84817a);
---
+++ lib/font_style.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:cashy_app/custom_colors.dart';
+
+TextStyle mainHeader = TextStyle(
+    fontSize: 26,
+    color: darkGreen,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w700);
+
+TextStyle subHeader = TextStyle(
+    fontSize: 16,
+    color: Colors.grey,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.w300);
